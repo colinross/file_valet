58d4599cc3c50d68d6583e1c7c40fe07c5dae6a6
Colin Ross <colinross@gmail.com>
2019-10-02 12:50:24 -0700

    feat: set uuid if blank and column attribute exists

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
+require 'securerandom'
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  before_save do
+    self.uuid ||= SecureRandom.uuid if attributes.include?("uuid")
+  end
 end
