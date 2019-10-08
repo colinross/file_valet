b04905018ce40707c001dcf64765e673b22230b0
Colin Ross <colinross@gmail.com>
2019-10-08 13:49:16 -0700

    feature: Clean up UI with a FileManager using tiles

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
-    <h1>{{ title }}</h1>
-    <slot></slot>
+    <div class="container">
+      <h1 class="title">{{ title }}</h1>
+      <h2 class="subtitle">{{ subtitle }}</h2>
+        <slot></slot>      
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  props: ["title"]
+  props: ["title", "subtitle"]
 }
 </script>
 
