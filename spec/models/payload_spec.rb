33987da2c6c2083c04910b5a0004c13b5c1fe4d3
Colin Ross <colinross@gmail.com>
2019-10-08 12:24:10 -0700

    feature: Add QR Code to payloads

--- spec/models/payload_spec.rb
+++ spec/models/payload_spec.rb
@@ -10,4 +10,23 @@ RSpec.describe Payload, type: :model do
   describe 'label' do
     it { is_expected.to validate_uniqueness_of(:label).allow_nil }
   end
+
+  describe 'qr_code' do
+    let(:record) { FactoryBot.create(:payload) }
+    it { expect(record.qr_code).to be_a ChunkyPNG::Image }
+    it 'can be transformed to a data_url' do
+      data_url = record.qr_code.to_data_url
+      expect(data_url).to be_a String
+      expect(data_url).to include('data:image/png;base64')
+    end
+
+    context 'error in creation' do
+      before do
+        dbl = double("QRCode")
+        expect(dbl).to receive(:as_png).and_raise StandardError.new("The flotsam and jitsum mixed!")
+        expect(RQRCode::QRCode).to receive(:new).and_return(dbl)
+      end
+      it { expect(record.qr_code).to be_nil }
+    end
+  end
 end
